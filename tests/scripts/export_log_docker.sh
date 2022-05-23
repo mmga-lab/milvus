ba65c54c78766545fd28b2e0a5b76516906d795c
zhuwenxing <wenxing.zhu@zilliz.com>
2022-05-23 09:45:23 +0800

    [skip e2e]Add script to export containers log one by one
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/scripts/export_log_docker.sh
+++ tests/scripts/export_log_docker.sh
@@ -13,7 +13,12 @@ fi
 echo "export logs start"
 for container in ${array[*]}
 do
-echo "export logs for container $container "
-docker logs $container > ./$log_dir/$container.log 2>&1 || echo "export logs for container $container failed"
+if [[ $container == milvus* ]];
+then
+    echo "export logs for container $container "
+    docker logs $container > ./$log_dir/$container.log 2>&1 || echo "export logs for container $container failed"
+else
+    echo "skip $container"
+fi
 done
 echo "export logs done"
