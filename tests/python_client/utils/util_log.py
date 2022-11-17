477d1083554353f6e9f0ab78b6cc85cd21a67cd1
zhuwenxing <wenxing.zhu@zilliz.com>
2022-11-17 19:50:29 +0800

    Update log to loguru
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/utils/util_log.py
+++ tests/python_client/utils/util_log.py
@@ -1,52 +1,37 @@
-import logging
+from loguru import logger
 import sys
 
 from config.log_config import log_config
 
 
 class TestLog:
-    def __init__(self, logger, log_debug, log_file, log_err, log_worker):
-        self.logger = logger
+    def __init__(self, log_debug, log_info, log_err, log_worker):
         self.log_debug = log_debug
-        self.log_file = log_file
+        self.log_info = log_info
         self.log_err = log_err
         self.log_worker = log_worker
+        self.log = logger
 
-        self.log = logging.getLogger(self.logger)
-        self.log.setLevel(logging.DEBUG)
-
+        config = {
+            self.log_debug: "DEBUG",
+            self.log_info: "INFO",
+            self.log_err: "ERROR",
+            self.log_worker: "DEBUG"
+        }
         try:
-            formatter = logging.Formatter("[%(asctime)s - %(levelname)s - %(name)s]: "
-                                          "%(message)s (%(filename)s:%(lineno)s)")
-            # [%(process)s] process NO.
-            dh = logging.FileHandler(self.log_debug)
-            dh.setLevel(logging.DEBUG)
-            dh.setFormatter(formatter)
-            self.log.addHandler(dh)
+            encoding = "utf-8"
+            enqueue = True
+            for file, level in config.items():
+                self.log.add(
+                    sink=file, level=level, encoding=encoding,
+                    enqueue=enqueue, rotation="500MB", retention="1 week",
+                    # colorize=True
+                )
 
-            fh = logging.FileHandler(self.log_file)
-            fh.setLevel(logging.INFO)
-            fh.setFormatter(formatter)
-            self.log.addHandler(fh)
-
-            eh = logging.FileHandler(self.log_err)
-            eh.setLevel(logging.ERROR)
-            eh.setFormatter(formatter)
-            self.log.addHandler(eh)
-
-            if self.log_worker != "":
-                wh = logging.FileHandler(self.log_worker)
-                wh.setLevel(logging.DEBUG)
-                wh.setFormatter(formatter)
-                self.log.addHandler(wh)
-
-            ch = logging.StreamHandler(sys.stdout)
-            ch.setLevel(logging.DEBUG)
-            ch.setFormatter(formatter)
-            # self.log.addHandler(ch)
+            self.log.add(sys.stdout, level="DEBUG", colorize=False)
 
         except Exception as e:
-            print("Can not use %s or %s or %s to log. error : %s" % (log_debug, log_file, log_err, str(e)))
+            print("Can not use %s or %s or %s to log. error : %s" % (log_debug, log_info, log_err, str(e)))
 
 
 """All modules share this unified log"""
@@ -54,4 +39,4 @@ log_debug = log_config.log_debug
 log_info = log_config.log_info
 log_err = log_config.log_err
 log_worker = log_config.log_worker
-test_log = TestLog('ci_test', log_debug, log_info, log_err, log_worker).log
+test_log = TestLog(log_debug, log_info, log_err, log_worker).log
