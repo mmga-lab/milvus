da2dc76cfc670693a1504d42b44ebcb47d342e48
zhuwenxing <wenxing.zhu@zilliz.com>
2022-12-11 06:52:02 +0800

    Update
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/restful_client/testcases/test_e2e.py
+++ tests/restful_client/testcases/test_e2e.py
@@ -1,3 +1,16 @@
 from api.collection import Collection
 
 
+class TestDefault:
+
+
+    def test_e2e(self):
+        collection = Collection()
+        payload = {
+            "collection_name": "test",
+            "dimension": 128,
+            "index_file_size": 1024,
+            "metric_type": "L2"
+        }
+        collection.create_collection(payload)
+        collection.drop_collection(payload)
