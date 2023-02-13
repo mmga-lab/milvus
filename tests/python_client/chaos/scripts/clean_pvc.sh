7553e9ebe276e4f076120db10032faa0a67d142c
zhuwenxing <wenxing.zhu@zilliz.com>
2023-02-13 19:33:41 +0800

    [test]Test delete topic
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/chaos/scripts/clean_pvc.sh
+++
@@ -1,7 +0,0 @@
-
-# Exit immediately for non zero status
-set -e
-release=${1:-"milvus-chaos"}
-ns=${2:-"chaos-testing"}
-kubectl delete pvc -l release=${release} -n=${ns} || echo "pvc with label release=${release} not found"
-kubectl delete pvc -l app.kubernetes.io/instance=${release} -n=${ns} || echo "pvc with label app.kubernetes.io/instance=${release} not found"
