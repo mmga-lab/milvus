8f7f9ee120945540d0ae6ad8fae78e2cd490d3df
zhuwenxing <wenxing.zhu@zilliz.com>
2023-08-09 16:50:54 +0800

    [test]Update standby test
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/chaos/testcases/test_single_request_operation_for_standby.py
+++ tests/python_client/chaos/testcases/test_single_request_operation_for_standby.py
@@ -1,5 +1,4 @@
 import pytest
-import threading
 from time import sleep
 from pymilvus import connections
 from chaos.checker import (CreateChecker,
@@ -9,17 +8,15 @@ from chaos.checker import (CreateChecker,
                            QueryChecker,
                            IndexChecker,
                            DeleteChecker,
+                           DropChecker,
                            Op)
 from utils.util_log import test_log as log
 from chaos import chaos_commons as cc
 from common.common_type import CaseLabel
-from common.milvus_sys import MilvusSys
+from common import common_func as cf
 from chaos.chaos_commons import assert_statistic
 from chaos import constants
 from delayed_assert import assert_expectations
-from utils.util_k8s import (get_milvus_instance_name,
-                            get_milvus_deploy_tool,
-                            reset_healthy_checker_after_standby_activated)
 
 
 class TestBase:
@@ -38,7 +35,7 @@ class TestBase:
 class TestOperations(TestBase):
 
     @pytest.fixture(scope="function", autouse=True)
-    def connection(self, host, port, user, password, milvus_ns):
+    def connection(self, host, port, user, password, minio_host):
         if user and password:
             # log.info(f"connect to {host}:{port} with user {user} and password {password}")
             connections.connect('default', host=host, port=port, user=user, password=password, secure=True)
@@ -51,52 +48,62 @@ class TestOperations(TestBase):
         self.port = port
         self.user = user
         self.password = password
-        self.milvus_sys = MilvusSys(alias='default')
-        self.chaos_ns = constants.CHAOS_NAMESPACE
-        self.milvus_ns = milvus_ns
-        self.release_name = get_milvus_instance_name(self.milvus_ns, milvus_sys=self.milvus_sys)
-        self.deploy_by = get_milvus_deploy_tool(self.milvus_ns, self.milvus_sys)
+        self.minio_endpoint = f"{minio_host}:9000"
 
     def init_health_checkers(self, collection_name=None):
         c_name = collection_name
+        schema = cf.gen_default_collection_schema(auto_id=False)
+
         checkers = {
-            Op.create: CreateChecker(collection_name=c_name),
-            Op.insert: InsertChecker(collection_name=c_name),
-            Op.flush: FlushChecker(collection_name=c_name),
-            Op.index: IndexChecker(collection_name=c_name),
-            Op.search: SearchChecker(collection_name=c_name),
-            Op.query: QueryChecker(collection_name=c_name),
-            Op.delete: DeleteChecker(collection_name=c_name),
+            Op.create: CreateChecker(collection_name=None, schema=schema),
+            Op.insert: InsertChecker(collection_name=c_name, schema=schema),
+            Op.flush: FlushChecker(collection_name=c_name, schema=schema),
+            Op.index: IndexChecker(collection_name=None, schema=schema),
+            Op.search: SearchChecker(collection_name=c_name, schema=schema),
+            Op.query: QueryChecker(collection_name=c_name, schema=schema),
+            Op.delete: DeleteChecker(collection_name=c_name, schema=schema),
+            Op.drop: DropChecker(collection_name=None, schema=schema)
         }
         self.health_checkers = checkers
 
     @pytest.mark.tags(CaseLabel.L3)
-    def test_operations(self, request_duration, target_component, is_check):
+    def test_operations(self, request_duration, is_check):
         # start the monitor threads to check the milvus ops
         log.info("*********************Test Start**********************")
         log.info(connections.get_connection_addr('default'))
         c_name = None
         self.init_health_checkers(collection_name=c_name)
         cc.start_monitor_threads(self.health_checkers)
-        log.info("*********************Load Start**********************")
         # wait request_duration
         request_duration = request_duration.replace("h", "*3600+").replace("m", "*60+").replace("s", "")
         if request_duration[-1] == "+":
             request_duration = request_duration[:-1]
         request_duration = eval(request_duration)
-        # start a thread to reset health_checkers when standby is activated.
-        t = threading.Thread(target=reset_healthy_checker_after_standby_activated,
-                             args=(self.milvus_ns, self.release_name, target_component, self.health_checkers),
-                             kwargs={"timeout": request_duration//2},
-                             daemon=True)
-        t.start()
-        # t.join()
-        log.info('start a thread to reset health_checkers when standby is activated')
         for i in range(10):
-            sleep(request_duration//10)
+            sleep(request_duration // 10)
             for k, v in self.health_checkers.items():
                 v.check_result()
+        for k, v in self.health_checkers.items():
+            v.pause()
+        for k, v in self.health_checkers.items():
+            v.check_result()
+        for k, v in self.health_checkers.items():  
+            log.info(f"{k} failed request: {v.fail_records}")
+        for k, v in self.health_checkers.items():  
+            log.info(f"{k} rto: {v.get_rto()}")
+
         if is_check:
-            assert_statistic(self.health_checkers)
+            assert_statistic(self.health_checkers, succ_rate_threshold=0.98)
             assert_expectations()
+            # get each checker's rto
+            for k, v in self.health_checkers.items():
+                log.info(f"{k} rto: {v.get_rto()}")
+                rto = v.get_rto()
+                pytest.assume(rto < 30,  f"{k} rto expect 30s but get {rto}s")  # rto should be less than 30s
+
+            if Op.insert in self.health_checkers:
+                # verify the no insert data loss
+                log.info("*********************Verify Data Completeness**********************")
+                self.health_checkers[Op.insert].verify_data_completeness()
+
         log.info("*********************Chaos Test Completed**********************")
