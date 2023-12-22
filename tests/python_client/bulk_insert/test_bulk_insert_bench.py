76e0f85bb25bf6622ce03d8d43c35db65805ce6d
zhuwenxing <wenxing.zhu@zilliz.com>
2023-12-22 10:40:19 +0800

    update
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/python_client/bulk_insert/test_bulk_insert_bench.py
+++ tests/python_client/bulk_insert/test_bulk_insert_bench.py
@@ -87,7 +87,7 @@ class TestBulkInsertPerf(TestcaseBaseBulkInsert):
             cf.gen_json_field(name=df.json_field),
             cf.gen_array_field(name=df.array_int_field, element_type=DataType.INT64),
             cf.gen_array_field(name=df.array_float_field, element_type=DataType.FLOAT),
-            cf.gen_array_field(name=df.array_string_field, element_type=DataType.VARCHAR),
+            cf.gen_array_field(name=df.array_string_field, element_type=DataType.VARCHAR, max_length=100),
             cf.gen_array_field(name=df.array_bool_field, element_type=DataType.BOOL),
             cf.gen_float_vec_field(name=df.vec_field, dim=dim),
         ]
@@ -146,7 +146,7 @@ class TestBulkInsertPerf(TestcaseBaseBulkInsert):
             cf.gen_json_field(name=df.json_field),
             cf.gen_array_field(name=df.array_int_field, element_type=DataType.INT64),
             cf.gen_array_field(name=df.array_float_field, element_type=DataType.FLOAT),
-            cf.gen_array_field(name=df.array_string_field, element_type=DataType.VARCHAR),
+            cf.gen_array_field(name=df.array_string_field, element_type=DataType.VARCHAR, max_length=100),
             cf.gen_array_field(name=df.array_bool_field, element_type=DataType.BOOL),
             cf.gen_float_vec_field(name=df.vec_field, dim=dim),
         ]
